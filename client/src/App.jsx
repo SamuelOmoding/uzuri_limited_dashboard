d9f538643e05418b9024cb8f09e9967ea01d9586
Omoding-web <samuel.omoding@student.moringaschool.com>
2025-03-13 21:09:14 +0300

    Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useLocation } from "react-router-dom";
 import { UpdateTheme, UseTheme } from "./Theme";
 import SideBar from "./components/SideBar";
@@ -13,10 +14,6 @@ import Reports from "./components/Reports";
 import ClientList from "./components/ClientList";
 import ServicesList from "./components/ServicesList";
 import Bg from "../src/assets/water.png";
-import { QueryClient, QueryClientProvider } from "react-query"; // For React Query
-
-// Create a React Query client
-const queryClient = new QueryClient();
 
 function App() {
   const darkTheme = UseTheme();
@@ -35,133 +32,40 @@ function App() {
     : "bg-gradient-to-r from-gray-500 to-gray-900";
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="app overflow-hidden" style={ThemeStyles}>
-        <SideBar toggleColor={toggleColor} darkTheme={darkTheme} />
-        <main className="content">
-          <TopNav toggleColor={toggleColor} darkTheme={darkTheme} />
+    <div className="app overflow-hidden" style={ThemeStyles}>
+      <SideBar toggleColor={toggleColor} darkTheme={darkTheme} />
+      <main className="content">
+        <TopNav toggleColor={toggleColor} darkTheme={darkTheme} />
 
-          {/* Conditionally render the welcome message */}
-          {location.pathname === "/" && (
-            <div className="flex items-center justify-center min-h-screen">
-              <h1
-                className={`px-10 py-25 mt-9 font-bold text-7xl text-transparent bg-clip-text ${headerGradient}`}
-              >
-                UZURI LIMITED DASHBOARD
-              </h1>
-            </div>
-          )}
+        {/* Conditionally render the welcome message */}
+        {location.pathname === "/" && (
+          <div className="flex items-center justify-center min-h-screen">
+            <h1
+              className={`px-10 py-25 mt-9 font-bold text-7xl text-transparent bg-clip-text ${headerGradient}`}
+            >
+              UZURI LIMITED DASHBOARD
+            </h1>
+          </div>
+        )}
 
-          {/* Routes */}
-          <Routes>
-            <Route
-              path="/Profile"
-              element={<Authentication ThemeStyles={ThemeStyles} />}
-            />
-            <Route
-              path="/Dashboard"
-              element={<Dashboard ThemeStyles={ThemeStyles} />}
-            />
-            <Route
-              path="/RegisterClient"
-              element={<RegisterClient ThemeStyles={ThemeStyles} />}
-            />
-            <Route
-              path="/FeeCalculator"
-              element={<FeeCalculator ThemeStyles={ThemeStyles} />}
-            />
-            <Route path="/Home" element={<Home />} />
-            <Route
-              path="/Invoice"
-              element={<Invoice ThemeStyles={ThemeStyles} />}
-            />
-            <Route path="/Reports" element={<Reports />} />
-            <Route
-              path="/ClientList"
-              element={<ClientList ThemeStyles={ThemeStyles} />}
-            />
-            <Route
-              path="/ServicesList"
-              element={<ServicesList ThemeStyles={ThemeStyles} />}
-            />
-          </Routes>
-        </main>
-      </div>
-    </QueryClientProvider>
+        {/* Routes */}
+        <Routes>
+          <Route path="/Profile" element={<Authentication ThemeStyles={ThemeStyles} />} />
+          <Route path="/Dashboard" element={<Dashboard ThemeStyles={ThemeStyles} />} />
+          <Route path="/RegisterClient" element={<RegisterClient ThemeStyles={ThemeStyles} />} />
+          <Route path="/FeeCalculator" element={<FeeCalculator ThemeStyles={ThemeStyles} />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Invoice" element={<Invoice ThemeStyles={ThemeStyles} />} />
+          <Route path="/Reports" element={<Reports />} />
+          <Route path="/ClientList" element={<ClientList ThemeStyles={ThemeStyles} />} />
+          <Route path="/ServicesList" element={<ServicesList ThemeStyles={ThemeStyles} />} />
+        </Routes>
+        {/* <ServicesList style={ThemeStyles} /> */}
+      </main>
+    </div>
   );
 }
 
 export default App;
-
-
-// import React from 'react';
-// import { useLocation } from "react-router-dom";
-// import { UpdateTheme, UseTheme } from "./Theme";
-// import SideBar from "./components/SideBar";
-// import TopNav from "./components/TopNav";
-// import { Routes, Route } from "react-router-dom";
-// import Authentication from './components/Authentication';
-// import Invoice from "./components/Invoice";
-// import Dashboard from "./components/Dashboard";
-// import RegisterClient from "./components/RegisterClient";
-// import FeeCalculator from "./components/FeeCalculator";
-// import Home from "./components/Home";
-// import Reports from "./components/Reports";
-// import ClientList from "./components/ClientList";
-// import ServicesList from "./components/ServicesList";
-// import Bg from "../src/assets/water.png";
-
-// function App() {
-//   const darkTheme = UseTheme();
-//   const toggleColor = UpdateTheme();
-//   const location = useLocation();
-
-//   const ThemeStyles = {
-//     backgroundImage: `url(${Bg})`,
-//     backgroundColor: darkTheme ? "rgb(46, 45, 45)" : "rgb(171, 163, 159)",
-//     color: darkTheme ? "rgb(240, 240, 240)" : "rgb(26, 46, 5)",
-//     backgroundSize: "cover",
-//   };
-
-//   const headerGradient = darkTheme
-//     ? "bg-gradient-to-r from-blue-900 to-gray-500"
-//     : "bg-gradient-to-r from-gray-500 to-gray-900";
-
-//   return (
-//     <div className="app overflow-hidden" style={ThemeStyles}>
-//       <SideBar toggleColor={toggleColor} darkTheme={darkTheme} />
-//       <main className="content">
-//         <TopNav toggleColor={toggleColor} darkTheme={darkTheme} />
-
-//         {/* Conditionally render the welcome message */}
-//         {location.pathname === "/" && (
-//           <div className="flex items-center justify-center min-h-screen">
-//             <h1
-//               className={`px-10 py-25 mt-9 font-bold text-7xl text-transparent bg-clip-text ${headerGradient}`}
-//             >
-//               UZURI LIMITED DASHBOARD
-//             </h1>
-//           </div>
-//         )}
-
-//         {/* Routes */}
-//         <Routes>
-//           <Route path="/Profile" element={<Authentication ThemeStyles={ThemeStyles} />} />
-//           <Route path="/Dashboard" element={<Dashboard ThemeStyles={ThemeStyles} />} />
-//           <Route path="/RegisterClient" element={<RegisterClient ThemeStyles={ThemeStyles} />} />
-//           <Route path="/FeeCalculator" element={<FeeCalculator ThemeStyles={ThemeStyles} />} />
-//           <Route path="/Home" element={<Home />} />
-//           <Route path="/Invoice" element={<Invoice ThemeStyles={ThemeStyles} />} />
-//           <Route path="/Reports" element={<Reports />} />
-//           <Route path="/ClientList" element={<ClientList ThemeStyles={ThemeStyles} />} />
-//           <Route path="/ServicesList" element={<ServicesList ThemeStyles={ThemeStyles} />} />
-//         </Routes>
-//         {/* <ServicesList style={ThemeStyles} /> */}
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
--- client/src/components/Authentication.jsx
+++ client/src/components/Authentication.jsx
@@ -6,10 +6,10 @@ import { useNavigate } from "react-router-dom";
 
 function Login({ switchForm, ThemeStyles }) {
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false); // Loading state
   const { setUser } = useUser();
- 
   const navigate = useNavigate();
-  
+
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -22,33 +22,26 @@ function Login({ switchForm, ThemeStyles }) {
         .required("Required"),
     }),
     onSubmit: (values, { setSubmitting }) => {
-      console.log(values);
+      setLoading(true); // Start loading
       fetch("https://uzuri-limited-backend-veim.onrender.com/api/auth/admin_login", {
         method: "POST",
-                headers: {
+        headers: {
           "Content-Type": "application/json",
           'Access-Control-Allow-Origin': '*',
         },
         body: JSON.stringify(values),
-
       })
         .then((res) => res.json())
         .then(() => {
           navigate("/dashboard");
-        //   console.log(users);
-        //   if(Array.isArray(users)) {
-        //   const user = users.find(
-        //     (u) => u.email === values.email && u.password === values.password
-        //   );
-        //   if (user) {
-        //     console.log("Login successful");
-        //     setUser(user);
-        //     navigate("/dashboard");
-        //   } else {
-        //     console.log("Invalid credentials");
-        //   }
-        //   setSubmitting(false);
-         });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        })
+        .finally(() => {
+          setLoading(false); // Stop loading
+          setSubmitting(false);
+        });
     },
   });
 
@@ -120,9 +113,10 @@ function Login({ switchForm, ThemeStyles }) {
           <div className="mt-4 flex items-center justify-between">
             <button
               type="submit"
+              disabled={loading} // Disable button while loading
               className="rounded-md bg-blue-600 px-4 py-1 text-lg font-semibold text-white shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
-              Login
+              {loading ? "Loading..." : "Login"} {/* Show loading text */}
             </button>
             <p
               className="text-blue-600 cursor-pointer hover:underline"
@@ -139,6 +133,7 @@ function Login({ switchForm, ThemeStyles }) {
 
 function Register({ switchForm, ThemeStyles }) {
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false); // Loading state
 
   const formik = useFormik({
     initialValues: {
@@ -158,7 +153,7 @@ function Register({ switchForm, ThemeStyles }) {
         .required("Required"),
     }),
     onSubmit: (values, { setSubmitting }) => {
-      console.log(values);
+      setLoading(true); // Start loading
       fetch("https://uzuri-limited-backend-veim.onrender.com/api/auth/admin_signup", {
         method: "POST",
         headers: {
@@ -170,8 +165,14 @@ function Register({ switchForm, ThemeStyles }) {
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
-          setSubmitting(false);
           switchForm(); // Switch to login form
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        })
+        .finally(() => {
+          setLoading(false); // Stop loading
+          setSubmitting(false);
         });
     },
   });
@@ -236,9 +237,10 @@ function Register({ switchForm, ThemeStyles }) {
           <div className="mt-4 flex items-center justify-between">
             <button
               type="submit"
+              disabled={loading} // Disable button while loading
               className="rounded-md bg-blue-600 px-4 py-1 text-lg font-semibold text-white shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
-              Register
+              {loading ? "Loading..." : "Register"} {/* Show loading text */}
             </button>
             <p
               className="text-blue-600 cursor-pointer hover:underline"
@@ -272,3 +274,279 @@ function Form() {
 }
 
 export default Form;
+
+
+// import { useState } from "react";
+// import { useFormik } from "formik";
+// import * as Yup from "yup";
+// import { useUser } from "../UserContext";
+// import { useNavigate } from "react-router-dom";
+
+// function Login({ switchForm, ThemeStyles }) {
+//   const [showPassword, setShowPassword] = useState(false);
+//   const { setUser } = useUser();
+ 
+//   const navigate = useNavigate();
+  
+//   const formik = useFormik({
+//     initialValues: {
+//       email: "",
+//       password: "",
+//     },
+//     validationSchema: Yup.object({
+//       email: Yup.string().email("Invalid email address").required("Required"),
+//       password: Yup.string()
+//         .min(8, "Must be at least 8 characters")
+//         .required("Required"),
+//     }),
+//     onSubmit: (values, { setSubmitting }) => {
+//       console.log(values);
+//       fetch("https://uzuri-limited-backend-veim.onrender.com/api/auth/admin_login", {
+//         method: "POST",
+//                 headers: {
+//           "Content-Type": "application/json",
+//           'Access-Control-Allow-Origin': '*',
+//         },
+//         body: JSON.stringify(values),
+
+//       })
+//         .then((res) => res.json())
+//         .then(() => {
+//           navigate("/dashboard");
+//         //   console.log(users);
+//         //   if(Array.isArray(users)) {
+//         //   const user = users.find(
+//         //     (u) => u.email === values.email && u.password === values.password
+//         //   );
+//         //   if (user) {
+//         //     console.log("Login successful");
+//         //     setUser(user);
+//         //     navigate("/dashboard");
+//         //   } else {
+//         //     console.log("Invalid credentials");
+//         //   }
+//         //   setSubmitting(false);
+//          });
+//     },
+//   });
+
+//   return (
+//     <div
+//       className="flex flex-col items-center justify-center h-screen"
+//       style={{
+//         ...ThemeStyles,
+//       }}
+//     >
+//       <div className="max-w-md w-full bg-gray-500 shadow-lg rounded-lg mb-9">
+//         <form onSubmit={formik.handleSubmit} className="m-3">
+//           <h2 className="text-2xl font-medium py-2 border-b border-gray-500 text-gray-700 mb-4">
+//             Login Form
+//           </h2>
+//           <div className="flex flex-col items-center gap-y-4 mt-2 w-full">
+//             <div className="w-full">
+//               <label
+//                 htmlFor="email"
+//                 className="block text-lg font-medium leading-6"
+//               >
+//                 Email address <span className="text-red-500">*</span>
+//               </label>
+//               <input
+//                 id="email"
+//                 name="email"
+//                 type="email"
+//                 className="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+//                 onChange={formik.handleChange}
+//                 onBlur={formik.handleBlur}
+//                 value={formik.values.email}
+//               />
+//               {formik.touched.email && formik.errors.email ? (
+//                 <div className="text-red-500">{formik.errors.email}</div>
+//               ) : null}
+//             </div>
+//             <div className="w-full">
+//               <label
+//                 htmlFor="password"
+//                 className="block text-lg font-medium leading-6"
+//               >
+//                 Password <span className="text-red-500">*</span>
+//               </label>
+//               <input
+//                 id="password"
+//                 name="password"
+//                 type={showPassword ? "text" : "password"}
+//                 className="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+//                 onChange={formik.handleChange}
+//                 onBlur={formik.handleBlur}
+//                 value={formik.values.password}
+//               />
+//               {formik.touched.password && formik.errors.password ? (
+//                 <div className="text-red-500">{formik.errors.password}</div>
+//               ) : null}
+//             </div>
+//             <div className="w-full mt-2 flex items-center">
+//               <input
+//                 type="checkbox"
+//                 id="showPassword"
+//                 checked={showPassword}
+//                 onChange={() => setShowPassword(!showPassword)}
+//               />
+//               <label htmlFor="showPassword" className="ml-2 text-lg">
+//                 Show Password
+//               </label>
+//             </div>
+//           </div>
+//           <div className="mt-4 flex items-center justify-between">
+//             <button
+//               type="submit"
+//               className="rounded-md bg-blue-600 px-4 py-1 text-lg font-semibold text-white shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+//             >
+//               Login
+//             </button>
+//             <p
+//               className="text-blue-600 cursor-pointer hover:underline"
+//               onClick={switchForm}
+//             >
+//               Register
+//             </p>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Register({ switchForm, ThemeStyles }) {
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   const formik = useFormik({
+//     initialValues: {
+//       username: "",
+//       email: "",
+//       password: "",
+//       confirmPassword: "",
+//     },
+//     validationSchema: Yup.object({
+//       username: Yup.string().required("Required"),
+//       email: Yup.string().email("Invalid email address").required("Required"),
+//       password: Yup.string()
+//         .min(8, "Must be at least 8 characters")
+//         .required("Required"),
+//       confirmPassword: Yup.string()
+//         .oneOf([Yup.ref("password"), null], "Passwords must match")
+//         .required("Required"),
+//     }),
+//     onSubmit: (values, { setSubmitting }) => {
+//       console.log(values);
+//       fetch("https://uzuri-limited-backend-veim.onrender.com/api/auth/admin_signup", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           'Access-Control-Allow-Origin': '*',
+//         },
+//         body: JSON.stringify(values),
+//       })
+//         .then((res) => res.json())
+//         .then((data) => {
+//           console.log(data);
+//           setSubmitting(false);
+//           switchForm(); // Switch to login form
+//         });
+//     },
+//   });
+
+//   return (
+//     <div
+//       className="flex flex-col items-center justify-center h-screen"
+//       style={ThemeStyles}
+//     >
+//       <div className="max-w-md w-full bg-gray-500 shadow-lg rounded-lg p-2 mb-9">
+//         <form onSubmit={formik.handleSubmit} className="m-3">
+//           <h2 className="text-2xl font-medium py-2 border-b border-gray-300 text-gray-700 mb-4">
+//             Registration Form
+//           </h2>
+//           <div className="grid grid-cols-1 gap-4 mt-2">
+//             {["username", "email", "password", "confirmPassword"].map(
+//               (field) => (
+//                 <div key={field}>
+//                   <label
+//                     htmlFor={field}
+//                     className="block text-lg font-medium leading-6"
+//                   >
+//                     {field.charAt(0).toUpperCase() + field.slice(1)}{" "}
+//                     <span className="text-red-500">*</span>
+//                   </label>
+//                   <input
+//                     id={field}
+//                     name={field}
+//                     type={
+//                       field === "email"
+//                         ? "email"
+//                         : field === "password" || field === "confirmPassword"
+//                         ? showPassword
+//                           ? "text"
+//                           : "password"
+//                         : "text"
+//                     }
+//                     onChange={formik.handleChange}
+//                     onBlur={formik.handleBlur}
+//                     value={formik.values[field]}
+//                     required
+//                     className="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+//                   />
+//                   {formik.touched[field] && formik.errors[field] ? (
+//                     <div className="text-red-500">{formik.errors[field]}</div>
+//                   ) : null}
+//                 </div>
+//               )
+//             )}
+//             <div className="w-full mt-2 flex items-center">
+//               <input
+//                 type="checkbox"
+//                 id="showPassword"
+//                 checked={showPassword}
+//                 onChange={() => setShowPassword(!showPassword)}
+//               />
+//               <label htmlFor="showPassword" className="ml-2 text-lg">
+//                 Show Password
+//               </label>
+//             </div>
+//           </div>
+//           <div className="mt-4 flex items-center justify-between">
+//             <button
+//               type="submit"
+//               className="rounded-md bg-blue-600 px-4 py-1 text-lg font-semibold text-white shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+//             >
+//               Register
+//             </button>
+//             <p
+//               className="text-blue-600 cursor-pointer hover:underline"
+//               onClick={switchForm}
+//             >
+//               Login
+//             </p>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Form() {
+//   const [isLoginForm, setIsLoginForm] = useState(true);
+
+//   const switchForm = () => {
+//     setIsLoginForm((prev) => !prev);
+//   };
+
+//   return (
+//     <div>
+//       {isLoginForm ? (
+//         <Login switchForm={switchForm} />
+//       ) : (
+//         <Register switchForm={switchForm} />
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Form;
