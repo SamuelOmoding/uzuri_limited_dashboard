c3be79351a107c38ef343951151ddb9d9c262364
Omoding-web <samuel.omoding@student.moringaschool.com>
2025-03-14 21:38:42 +0300

    updates

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -6,14 +6,14 @@ import { Link } from 'react-router-dom';
 
 function TopNav({ toggleColor, darkTheme }) {
 
-const ThemeStyles = {
-  backgroundColor: darkTheme ? "#282A36" : "#D3D3D3",
-  color: darkTheme ? "#FFFFFF" : "#000000"
-};
+  const ThemeStyles = {
+    backgroundColor: darkTheme ? "#282A36" : "#D3D3D3",
+    color: darkTheme ? "#FFFFFF" : "#000000"
+  };
 
-const MenuStyles = {
-  backgroundColor: darkTheme ? "#282A36" : "#D3D3D3"
-};
+  const MenuStyles = {
+    backgroundColor: darkTheme ? "#282A36" : "#D3D3D3"
+  };
 
   return (
     <Box className="flex justify-between p-2" style={ThemeStyles}>
@@ -34,3 +34,41 @@ const MenuStyles = {
 }
 
 export default TopNav;
+
+
+// import { Box, IconButton } from '@mui/material';
+// import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+// import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+// import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+// import { Link } from 'react-router-dom';
+
+// function TopNav({ toggleColor, darkTheme }) {
+
+// const ThemeStyles = {
+//   backgroundColor: darkTheme ? "#282A36" : "#D3D3D3",
+//   color: darkTheme ? "#FFFFFF" : "#000000"
+// };
+
+// const MenuStyles = {
+//   backgroundColor: darkTheme ? "#282A36" : "#D3D3D3"
+// };
+
+//   return (
+//     <Box className="flex justify-between p-2" style={ThemeStyles}>
+//       <Box>
+//       </Box>
+//       <Box display="flex" p={2}>
+//         <IconButton style={{ ...MenuStyles, marginRight: '16px' }} className="text-xl" onClick={toggleColor}>
+//           {darkTheme ? <DarkModeOutlinedIcon /> : <LightModeOutlinedIcon />}
+//         </IconButton>
+//         <Link to="/home">
+//           <IconButton style={MenuStyles} className="text-neutral-300">
+//             <Person2OutlinedIcon />
+//           </IconButton>
+//         </Link>
+//       </Box>
+//     </Box>
+//   );
+// }
+
+// export default TopNav;
